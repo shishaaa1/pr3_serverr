4eb508fb8a99b16906d208136c6630d6fc8c22d5
danil <lalalamistikc31@gmail.com>
2025-10-28 16:29:44 +0500

    Создание класса Snakes в Common

---
+++ Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Common")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b94550ca-4e17-4c07-9951-474e5c283635")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Common/Snakes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Point(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+            public Point() { }
+        }
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Start
+        }
+        public List<Point> Points = new List<Point>();
+        public Direction direction = Direction.Start;
+        public bool GameOVer = false;
+    }
+}
